236467ef6ba99359c0c5c0e313d4a00f9160dc04
Pun <80048553+thammanant@users.noreply.github.com>
2024-02-06 20:33:21 +0700

    Add Inventory AbstractInventory Class and Add Command Interface

---
+++ src/AbstractInventory.java
@@ -0,0 +1,3 @@
+abstract class AbstractInventory implements Command{
+    public abstract void execute();
+}
---
+++ src/Inventory.java
@@ -0,0 +1,48 @@
+public class Inventory extends AbstractInventory{
+    private Book[] books;
+
+    private Command[] commands;
+
+    public void save() {
+        // TODO
+    }
+
+    public Book get_book_by_id(Integer id) {
+        // TODO
+        return null;
+    }
+
+    public Book get_book_by_name(String name) {
+        // TODO
+        return null;
+    }
+
+    public void add_book(Book book) {
+        // TODO
+    }
+
+    public void sell_book(Book book) {
+        // TODO
+    }
+
+    public void add_copies(Book book, Integer quantity) {
+        // TODO
+    }
+
+    public void change_price(Book book, Integer price) {
+        // TODO
+    }
+
+    public void add_command(Command command) {
+        // TODO
+    }
+
+    // Execute all commands
+    public void execute() {
+        // TODO
+    }
+
+    public void recover() {
+        // TODO
+    }
+}
