b0481bfc526f8e444a1599eaf3173a94601010cd
Pun <80048553+thammanant@users.noreply.github.com>
2024-02-06 22:36:12 +0700

    Add function to Inventory, Book and WriteToLog

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,16 +1,18 @@
+import java.util.Objects;
+
 public class Inventory extends AbstractInventory implements Originator{
     private Book[] books;
 
     private InventoryDecorator[] commands;
 
-    private Caretaker caretaker;
+    private final Caretaker caretaker;
 
     public Inventory() {
         caretaker = new Caretaker();
     }
 
     public Memento save() {
-        return new ConcreteMemento(this);
+        return new ConcreteMemento(this, caretaker);
     }
 
     public void recover(Memento memento) {
@@ -18,38 +20,77 @@ public class Inventory extends AbstractInventory implements Originator{
     }
 
     public Book get_book_by_id(Integer id) {
-        // TODO
+        for (Book b : books) {
+            if (Objects.equals(b.get_ID(), id)) {
+                return b;
+            }
+        }
         return null;
     }
 
     public Book get_book_by_name(String name) {
-        // TODO
+        for (Book b : books) {
+            if (b.get_Name().equals(name)) {
+                return b;
+            }
+        }
         return null;
     }
 
     public void add_book(String name, Integer price) {
-        // TODO
+        Book book = new Book(name, price);
+
+        // check if book already exists
+        for (Book b : books) {
+            if (b.get_Name().equals(name)) {
+                b.set_Quantity(b.get_Quantity() + 1);
+            } else {
+                Book[] newBooks = new Book[books.length + 1];
+                System.arraycopy(books, 0, newBooks, 0, books.length);
+                books = newBooks;
+                books[books.length - 1] = book;
+                book.set_ID(books.length);
+            }
+        }
     }
 
     public void sell_book(Integer id) {
-        // TODO
+        for (Book b : books) {
+            if (Objects.equals(b.get_ID(), id)) {
+                b.set_Quantity(b.get_Quantity() - 1);
+            }
+            else {
+                System.out.println("Out of stock!");
+            }
+        }
     }
 
-    
-
     public void add_copies(Integer id, Integer quantity) {
-        // TODO
+        for (Book b : books) {
+            if (Objects.equals(b.get_ID(), id)) {
+                b.set_Quantity(b.get_Quantity() + quantity);
+            }
+            else {
+                System.out.println("Book not found!");
+            }
+        }
     }
 
     public void change_price(Integer id, Integer price) {
-        // TODO
+        for (Book b : books) {
+            if (Objects.equals(b.get_ID(), id)) {
+                b.set_Price(price);
+            }
+            else {
+                System.out.println("Book not found!");
+            }
+        }
     }
 
     public void add_command(Command command) {
         // TODO
     }
 
-    // Execute all commands
     @Override
     public void execute() {
         // TODO
